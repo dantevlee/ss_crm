763117a9f03083b57c68f5319e7096fc8671fc3c
dantevlee <dante.leeseberg@gmail.com>
2024-04-14 21:18:09 -0500

    email column

---
+++ api/migrations/1711751044316_add-email-to-users-table.js
@@ -0,0 +1,14 @@
+
+exports.up = pgm => {
+  pgm.sql(`
+    ALTER TABLE "Users"
+    ADD COLUMN email VARCHAR(255) UNIQUE;
+  `);
+};
+
+exports.down = pgm => {
+  pgm.sql(`
+    ALTER TABLE "Users"
+    DROP COLUMN email;
+  `);
+};
