7df769a434898e29024d667cef2d24f102411a53
dantevlee <dante.leeseberg@gmail.com>
2024-04-15 17:27:34 -0500

    users API done

---
+++ api/migrations/1711751044318_add_tokens_table.js
@@ -0,0 +1,12 @@
+exports.up = pgm => {
+  pgm.createTable('Reset_Tokens',{
+    id: 'id',
+    email: { type: 'varchar(255)', notNull: true },
+    token: { type: 'text', notNull: true },
+    created_at: { type: 'timestamp', default: pgm.func('CURRENT_TIMESTAMP') },
+  })
+};
+
+exports.down = pgm => {
+  pgm.dropTable('Reset_Tokens')
+};
